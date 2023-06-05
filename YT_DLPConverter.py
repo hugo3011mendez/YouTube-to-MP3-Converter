d12a42a5cbf37f301b2d04139aa7abc5dc116ee8
Hugo <hugo3011mendez@gmail.com>
2023-06-05 20:38:52 +0200

    Updated output file modification date

--- YT_DLPConverter.py
+++ YT_DLPConverter.py
@@ -1,4 +1,5 @@
 import os
+import datetime
 import yt_dlp
 
 exit = False
@@ -27,7 +28,17 @@ while exit == False:
 
     # Conversion and download
     with yt_dlp.YoutubeDL(ydl_opts) as ydl:
+        info_dict = ydl.extract_info(url, download=False)
+        video_title = info_dict['title']
+        output_file = os.path.join(download_path, f'{video_title}.mp3')
+
         ydl.download([url])
+
+        # Get the current timestamp
+        current_time = datetime.datetime.now()
+
+        # Modify the file's modification time
+        os.utime(output_file, (current_time.timestamp(), current_time.timestamp()))
 
     # Question regarding the loop
     response = str(input("\033[1;34m Do you want to keep converting videos? y/n \n"))
